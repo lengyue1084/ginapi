fe102085d0491334114cfa0a3168c8c75878053a
jiuyue <95695864@qq.com>
2021-08-14 16:43:42 +0800

    日志写文件配置

--- internal/conf/log.go
+++ internal/conf/log.go
@@ -1,9 +1,48 @@
 package conf
 
-import "go.uber.org/zap"
+import (
+	"fmt"
+	"ginapi/tool"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
-func NewZap() *zap.Logger {
-	logger, err := zap.NewProduction()
+func NewZap(conf *Conf) *zap.Logger {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder, // 小写编码器
+		EncodeTime:     zapcore.ISO8601TimeEncoder,    // ISO8601 UTC 时间格式
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.FullCallerEncoder, // 全路径编码器
+	}
+	// 设置日志级别
+	atom := zap.NewAtomicLevelAt(zap.DebugLevel)
+	filename := conf.Conf.GetString("log.file.path") + "/" + tool.Date("Y-m-d") + ".log"
+	logType := conf.Conf.GetString("log.file.type")
+	if logType != "" {
+		logType = "json"
+	}
+	dev := conf.Conf.GetBool("log.file.prod")
+	config := zap.Config{
+		Level:            atom,                                                                           // 日志级别
+		Development:      dev,                                                                            // 开发模式，堆栈跟踪
+		Encoding:         logType,                                                                        // 输出格式 console 或 json
+		EncoderConfig:    encoderConfig,                                                                  // 编码器配置
+		InitialFields:    map[string]interface{}{"serviceName": conf.Conf.GetString("server.http.name")}, // 初始化字段，如：添加一个服务器名称
+		OutputPaths:      []string{"stdout", filename},                                                   // 输出到指定文件 stdout（标准输出，正常颜色） stderr（错误输出，红色）
+		ErrorOutputPaths: []string{"stderr"},
+	}
+	// 构建日志
+	logger, err := config.Build()
+	if err != nil {
+		panic(fmt.Sprintf("log 初始化失败: %v", err))
+	}
 	if err != nil {
 		panic(err)
 	}
