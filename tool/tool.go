62ee4686014129b35692d658ca1b549883b96f07
jiuyue <95695864@qq.com>
2021-08-14 16:44:19 +0800

    添加时间方法

--- tool/tool.go
+++ tool/tool.go
@@ -1,6 +1,9 @@
 package tool
 
-import "os"
+import (
+	"os"
+	"time"
+)
 
 func PathExists(path string) (bool, error) {
 	_, err := os.Stat(path)
@@ -11,4 +14,14 @@ func PathExists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+func Date(f string) string {
+	if f == "Y-m-d H:i:s" || f == "" {
+		return time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05")
+	}
+	if f == "Y-m-d" {
+		return time.Unix(time.Now().Unix(), 0).Format("2006-01-02")
+	}
+	return ""
 }
