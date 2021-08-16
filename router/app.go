3587666204574e297cf73754455b7c1a21c7df1a
jiuyue <95695864@qq.com>
2021-08-16 09:48:47 +0800

    修改gin mode为可配置

--- router/app.go
+++ router/app.go
@@ -1,21 +1,27 @@
 package router
 
 import (
+	"ginapi/internal/conf"
 	"ginapi/middleware"
 	"github.com/gin-gonic/gin"
 )
+
 type App struct {
 	app *gin.Engine
 }
 
 func NewApp(
 	middleware *middleware.Middleware,
+	conf *conf.Conf,
 ) *App {
+	if !conf.Conf.GetBool("server.dev") {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	r := gin.New()
 	//gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
 	//	log.Printf("endpoint %v %v %v %v\n", httpMethod, absolutePath, handlerName, nuHandlers)
 	//}
-	r.Use(middleware.Cors(),middleware.Logger(),middleware.Recovery())
+	r.Use(middleware.Cors(), middleware.Logger(), middleware.Recovery())
 	return &App{
 		app: r,
 	}
