4ac6bdd1c180ee5555bad22c0ced8bef494b2f80
jiuyue <95695864@qq.com>
2021-08-16 10:04:09 +0800

    添加HomeService路由

--- router/router.go
+++ router/router.go
@@ -5,14 +5,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/wire"
 )
-var ProviderSet = wire.NewSet(NewApp,NewRouter)
+
+var ProviderSet = wire.NewSet(NewApp, NewRouter)
 
 func NewRouter(
 	app *App,
-	UserServer *service.UserServer,
+	UserServer *service.UserService,
+	HomeServer *service.HomeService,
 ) *gin.Engine {
 	router := app.app.Group("/")
 	{
+		router.GET("/", HomeServer.Index)
 		router.GET("/login", UserServer.Login)
 	}
 	return app.app
